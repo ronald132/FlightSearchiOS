d7e922c42b64070dd2c961e2ecc7a3a3446a1702
ronald132 <ronald132@gmail.com>
2019-12-19 15:52:23 +0800

    Initial Commit

---
+++ Flight Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flight Search
+//
+//  Created by Ronald Situmorang on 19/12/19.
+//  Copyright © 2019 Ronald Situmorang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
